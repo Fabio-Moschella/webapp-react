9c6814384183c7769a750db69b6d69a17994db34
Fabio-Moschella <fabiomoschella1999@gmail.com>
2025-06-17 17:28:43 +0200

    add moviedetailpage

--- src/pages/MovieDetailPage.jsx
+++ src/pages/MovieDetailPage.jsx
@@ -1,3 +1,18 @@
-function MovieDetailPage() {}
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+function MovieDetailPage() {
+  const { id: movieId } = useParams();
+  const [movie, setMovie] = useState();
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/post/${movieId}`).then((res) => {
+      setMovie(res.data.results);
+    });
+  }, [movieId]);
+
+  return <div className="container my-5"></div>;
+}
 
 export default MovieDetailPage;
